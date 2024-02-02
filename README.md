7dd555853587bbe076f706c926064e136863e94c
JallyJumpus <jabelletsm@gmail.com>
2024-02-01 18:20:08 -0800

    Added dark/light mode and updated README file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# webblocker
+# BlockBuddy
+
+#### Description: 
+The Website Blocker Chrome Extension is a tool that empowers users to control their online productivity by blocking specific websites. Users can easily add sites to their blocklist and toggle between "Surf" mode and "Block" mode.
+
+
+#### Features: 
+
+- Block websites on your blocklist to enhance focus and productivity.
+- Toggle between "Surf" mode and "Block" mode.
+- Use the "Block Me" button for quick and easy blocking of the current site.
+- Enhance user experience by adding a dark mode for the extension interface.
+
+#### Future improvements: 
+
+- Timer Feature: Implement a timer feature. Only when the timer reaches zero can you exit the block mode and switch back to surf mode.
+- Customizations for Block Buddy: Introduce additional character customizations for Block Buddy, making the blocking experience more enjoyable.
+
+
+#### Video Demo:  URL [HERE](https://drive.google.com/file/d/117E-EXB-VjpUURpN1NVKbapP9tlWaFdT/view?usp=sharing)
--- popup.js
+++ popup.js
@@ -2,67 +2,92 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("toggleBtn");
   const siteInfo = document.getElementById("siteInfo");
   const blockButton = document.getElementById("blockButton");
+  const toggleModeBtn = document.getElementById("toggleModeBtn");
+  const modeIcon = document.getElementById("modeIcon");
 
   // Retrieve the current mode from storage only once when the DOM is loaded
-  chrome.storage.sync.get(["blockMode", "currentSite"], function (data) {
-    let isBlocked = data.blockMode !== undefined ? data.blockMode : true;
-    updateButtonText(isBlocked);
-    updateSiteInfo();
+  chrome.storage.sync.get(
+    ["blockMode", "currentSite", "darkMode"],
+    function (data) {
+      let isBlocked = data.blockMode !== undefined ? data.blockMode : true;
+      let isDarkMode = data.darkMode !== undefined ? data.darkMode : false;
 
-    // Toggle the blocked mode on click
-    toggleBtn.addEventListener("click", function () {
-      isBlocked = !isBlocked;
-
-      // Update the storage with the new mode
-      chrome.storage.sync.set({ blockMode: isBlocked });
-
-      // Update the button text
       updateButtonText(isBlocked);
+      updateSiteInfo();
+      updateDarkMode(isDarkMode);
 
-      // Notify content script to update the blocked sites logic
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
+      // Toggle the blocked mode on click
+      toggleBtn.addEventListener("click", function () {
+        isBlocked = !isBlocked;
 
-        // Check if content script is injected before sending the message
-        chrome.tabs.sendMessage(
-          activeTab.id,
-          { blockMode: isBlocked },
-          function (response) {
-            if (chrome.runtime.lastError) {
-              console.log(chrome.runtime.lastError);
-            }
+        // Update the storage with the new mode
+        chrome.storage.sync.set({ blockMode: isBlocked });
+
+        // Update the button text
+        updateButtonText(isBlocked);
+        updateDarkMode(isDarkMode);
+
+        // Notify content script to update the blocked sites logic
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            const activeTab = tabs[0];
+
+            // Check if content script is injected before sending the message
+            chrome.tabs.sendMessage(
+              activeTab.id,
+              { blockMode: isBlocked },
+              function (response) {
+                if (chrome.runtime.lastError) {
+                  console.log(chrome.runtime.lastError);
+                }
+              }
+            );
           }
         );
       });
-    });
 
-    // TODO: Fix this. Currently not working
-    // Add event listener to the Block button
-    blockButton.addEventListener("click", function () {
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const activeTab = tabs[0];
-        const currentSite = extractDomain(activeTab.url);
+      // Toggle dark mode on click
+      toggleModeBtn.addEventListener("click", function () {
+        isDarkMode = !isDarkMode;
+        chrome.storage.sync.set({ darkMode: isDarkMode });
+        updateDarkMode(isDarkMode);
+      });
 
-        // Update the storage with the new blocked site
-        chrome.storage.sync.get("blockedSites", function (data) {
-          const blockedSites = data.blockedSites || [];
+      // TODO: Fix this. Currently not working
+      // Add event listener to the Block button
+      blockButton.addEventListener("click", function () {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            const activeTab = tabs[0];
+            const currentSite = extractDomain(activeTab.url);
 
-          if (!blockedSites.includes(currentSite)) {
-            blockedSites.push(currentSite);
-            chrome.storage.sync.set({ blockedSites, currentSite }, function () {
-              console.log(`${currentSite} has been blocked.`);
+            // Update the storage with the new blocked site
+            chrome.storage.sync.get("blockedSites", function (data) {
+              const blockedSites = data.blockedSites || [];
+
+              if (!blockedSites.includes(currentSite)) {
+                blockedSites.push(currentSite);
+                chrome.storage.sync.set(
+                  { blockedSites, currentSite },
+                  function () {
+                    console.log(`${currentSite} has been blocked.`);
+                    // Update site info after blocking
+                    updateSiteInfo(currentSite);
+                  }
+                );
+              } else {
+                console.log(`${currentSite} is already blocked.`);
+              }
               // Update site info after blocking
               updateSiteInfo(currentSite);
             });
-          } else {
-            console.log(`${currentSite} is already blocked.`);
           }
-          // Update site info after blocking
-          updateSiteInfo(currentSite);
-        });
+        );
       });
-    });
-  });
+    }
+  );
 
   function updateButtonText(isBlocked) {
     toggleBtn.textContent = isBlocked ? "Focus Mode" : "Surf Mode";
@@ -79,5 +104,15 @@ document.addEventListener("DOMContentLoaded", function () {
       const currentSite = extractDomain(activeTab.url);
       siteInfo.textContent = `You are currently on ${currentSite}`;
     });
+  }
+
+  function updateDarkMode(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      modeIcon.src = "night-mode.png";
+    } else {
+      document.body.classList.remove("dark-mode");
+      modeIcon.src = "bright-mode.png";
+    }
   }
 });
