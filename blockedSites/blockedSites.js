72c6ef7c1fdf0c50e0e3bb2a3c498a530c0b3236
JallyJumpus <jabelletsm@gmail.com>
2024-02-21 17:54:05 -0800

    Fixed dynamic addition of current site to blocked list on 'Block' button press

--- blockedSites/blockedSites.js
+++ blockedSites/blockedSites.js
@@ -5,72 +5,75 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Add a click event listener to the button and redirect to popup page
   blockListButton.addEventListener("click", function () {
-    window.location.href = "../popup.html";
+    window.location.href = "../popup/popup.html";
   });
 
   // Fetch the list of blocked sites from Chrome storage
-  chrome.storage.sync.get(["blockMode", "darkMode"], function (data) {
-    const blockedSites = data.blockedSites || [];
-    let isDarkMode = data.darkMode !== undefined ? data.darkMode : false;
+  chrome.storage.sync.get(
+    ["blockMode", "darkMode", "blockedSites"],
+    function (data) {
+      const blockedSites = data.blockedSites || [];
+      let isDarkMode = data.darkMode !== undefined ? data.darkMode : false;
 
-    updateDarkMode(isDarkMode);
-
-    // Toggle dark mode on click
-    toggleDarkModeBtn.addEventListener("click", function () {
-      isDarkMode = !isDarkMode;
-      chrome.storage.sync.set({ darkMode: isDarkMode });
       updateDarkMode(isDarkMode);
-    });
 
-    // Check if there are blocked sites
-    if (blockedSites.length > 0) {
-      // Create an unordered list element
-      const ul = document.createElement("ul");
-
-      // Iterate over the blocked sites and create list items
-      blockedSites.forEach(function (site) {
-        const li = document.createElement("li");
-        li.textContent = site;
-
-        // Create delete icon
-        const deleteIcon = document.createElement("img");
-        deleteIcon.src = "../images/cross.png";
-        deleteIcon.alt = "Delete button";
-        deleteIcon.title = "Remove site from blocked list";
-        deleteIcon.classList.add("delete-icon");
-
-        // Add click event listener to the delete icon
-        deleteIcon.addEventListener("click", function () {
-          // Remove the corresponding site from the blockedSites array
-          const indexToRemove = blockedSites.indexOf(site);
-          if (indexToRemove !== -1) {
-            blockedSites.splice(indexToRemove, 1);
-
-            // Update Chrome storage with the updated blockedSites array
-            chrome.storage.sync.set({ blockedSites });
-
-            // Refresh the blocked list
-            blockList.innerHTML = "";
-            document.dispatchEvent(new Event("DOMContentLoaded"));
-          }
-        });
-
-        // Append the delete icon to the list item
-        li.appendChild(deleteIcon);
-
-        // Append the list item to the unordered list
-        ul.appendChild(li);
+      // Toggle dark mode on click
+      toggleDarkModeBtn.addEventListener("click", function () {
+        isDarkMode = !isDarkMode;
+        chrome.storage.sync.set({ darkMode: isDarkMode });
+        updateDarkMode(isDarkMode);
       });
 
-      // Replace the content of the blockList element with the dynamic list
-      blockList.innerHTML = "";
-      blockList.appendChild(ul);
-    } else {
-      // If there are no blocked sites, display a message
-      blockList.innerHTML =
-        "<p style='color: grey'>No sites are currently blocked.</p>";
+      // Check if there are blocked sites
+      if (blockedSites.length > 0) {
+        // Create an unordered list element
+        const ul = document.createElement("ul");
+
+        // Iterate over the blocked sites and create list items
+        blockedSites.forEach(function (site) {
+          const li = document.createElement("li");
+          li.textContent = site;
+
+          // Create delete icon
+          const deleteIcon = document.createElement("img");
+          deleteIcon.src = "../images/cross.png";
+          deleteIcon.alt = "Delete button";
+          deleteIcon.title = "Remove site from blocked list";
+          deleteIcon.classList.add("delete-icon");
+
+          // Add click event listener to the delete icon
+          deleteIcon.addEventListener("click", function () {
+            // Remove the corresponding site from the blockedSites array
+            const indexToRemove = blockedSites.indexOf(site);
+            if (indexToRemove !== -1) {
+              blockedSites.splice(indexToRemove, 1);
+
+              // Update Chrome storage with the updated blockedSites array
+              chrome.storage.sync.set({ blockedSites });
+
+              // Refresh the blocked list
+              blockList.innerHTML = "";
+              document.dispatchEvent(new Event("DOMContentLoaded"));
+            }
+          });
+
+          // Append the delete icon to the list item
+          li.appendChild(deleteIcon);
+
+          // Append the list item to the unordered list
+          ul.appendChild(li);
+        });
+
+        // Replace the content of the blockList element with the dynamic list
+        blockList.innerHTML = "";
+        blockList.appendChild(ul);
+      } else {
+        // If there are no blocked sites, display a message
+        blockList.innerHTML =
+          "<p style='color: grey'>No sites are currently blocked.</p>";
+      }
     }
-  });
+  );
 
   function updateDarkMode(isDarkMode) {
     if (isDarkMode) {
---
+++ popup/popup.js
@@ -0,0 +1,127 @@
+console.log("Popup.js is running");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("toggleBtn");
+  const siteInfo = document.getElementById("siteInfo");
+  const blockButton = document.getElementById("blockButton");
+  const toggleDarkModeBtn = document.getElementById("toggleModeBtn");
+  const themeIcon = document.getElementById("themeIcon");
+  const blockListButton = document.getElementById("blockListBtn");
+
+  // Add a click event listener to the button
+  blockListButton.addEventListener("click", function () {
+    // Redirect to the desired HTML page
+    window.location.href = "../blockedSites/blockedSites.html";
+  });
+
+  // Retrieve the current mode from storage only once when the DOM is loaded
+  chrome.storage.sync.get(
+    ["blockMode", "currentSite", "darkMode"],
+    function (data) {
+      let isBlocked = data.blockMode !== undefined ? data.blockMode : true;
+      let isDarkMode = data.darkMode !== undefined ? data.darkMode : false;
+
+      updateButtonText(isBlocked);
+      updateSiteInfo();
+      updateDarkMode(isDarkMode);
+
+      // Toggle the blocked mode on click
+      toggleBtn.addEventListener("click", function () {
+        isBlocked = !isBlocked;
+        console.log("isBlocked?", isBlocked);
+
+        // Update the storage with the new mode
+        chrome.storage.sync.set({ blockMode: isBlocked });
+
+        // Update the button text
+        updateButtonText(isBlocked);
+        updateDarkMode(isDarkMode);
+
+        // // Notify background script to update the blocked sites logic
+        // chrome.runtime.sendMessage({ blockMode: isBlocked });
+
+        // Notify content script to update the blocked sites logic
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            const activeTab = tabs[0];
+
+            // Check if content script is injected before sending the message
+            chrome.tabs.sendMessage(
+              activeTab.id,
+              { blockMode: isBlocked },
+              function (response) {
+                if (chrome.runtime.lastError) {
+                  console.log(chrome.runtime.lastError);
+                }
+              }
+            );
+          }
+        );
+      });
+
+      // Toggle dark mode on click
+      toggleDarkModeBtn.addEventListener("click", function () {
+        isDarkMode = !isDarkMode;
+        chrome.storage.sync.set({ darkMode: isDarkMode });
+        updateDarkMode(isDarkMode);
+      });
+
+      // TODO: Fix this. Currently not working
+      // Add event listener to the Block button
+      blockButton.addEventListener("click", function () {
+        chrome.tabs.query(
+          { active: true, currentWindow: true },
+          function (tabs) {
+            const activeTabURL = tabs[0].url;
+            const currentSite = extractDomain(activeTabURL);
+
+            // Update the storage with the new blocked site
+            chrome.storage.sync.get("blockedSites", function (data) {
+              const blockedSites = data.blockedSites || [];
+
+              if (!blockedSites.includes(currentSite)) {
+                blockedSites.push(currentSite);
+                chrome.storage.sync.set({ blockedSites }, function () {
+                  console.log(`${currentSite} has been blocked.`);
+
+                  // Update site info after blocking
+                  updateSiteInfo(currentSite);
+                });
+              } else {
+                console.log(`${currentSite} is already blocked.`);
+              }
+            });
+          }
+        );
+      });
+    }
+  );
+
+  function updateButtonText(isBlocked) {
+    toggleBtn.textContent = isBlocked ? "Focus Mode" : "Surf Mode";
+  }
+
+  function extractDomain(url) {
+    const hostname = new URL(url).hostname;
+    return hostname.replace(/^www\./, "").replace(/\.com$/, "");
+  }
+
+  function updateSiteInfo() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      const currentSite = extractDomain(activeTab.url);
+      siteInfo.textContent = `You are currently on ${currentSite}`;
+    });
+  }
+
+  function updateDarkMode(isDarkMode) {
+    if (isDarkMode) {
+      document.body.classList.add("dark-mode");
+      themeIcon.src = "../images/night-mode.png";
+    } else {
+      document.body.classList.remove("dark-mode");
+      themeIcon.src = "../images/bright-mode.png";
+    }
+  }
+});
